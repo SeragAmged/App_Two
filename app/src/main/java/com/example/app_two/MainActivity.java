8c26a72beb96c6be6112ec5251380b8c51387495
hello <asmaa.hamdy123@gmail.com>
2022-11-12 17:06:04 +0200

    update2

--- app/src/main/java/com/example/app_two/MainActivity.java
+++ app/src/main/java/com/example/app_two/MainActivity.java
@@ -11,20 +11,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public int counter(int num){
-        return num*52;
+
+    public int counter(int num) {
+        return num * 52;
     }
 
-    public double no(double a , double b){
-        return a/b;
+    public double no(double a, double b) {
+        return a / b;
     }
 
-    public double yes(double a , double b){
-        return a/b;
+    public double yes(double a, double b) {
+        return a / b;
     }
-    public double yesss(double a){return a+122;}
 
-    public double ragab(double a){return a+26;}
+    public double yesss(double a) {
+        return a + 122;
+    }
 
-    public double hello(double a){return a+90;}
+    public double ragab(double a) {
+        return a + 26;
+    }
 
+    public double hello(double a) {
+        return a + 90;
+    }
+
+}
